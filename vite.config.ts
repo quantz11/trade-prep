b50ab496ad0e031b96b507da586121fc92932c14
Quantz <gdbayucan@live.com.ph>
2026-08-19 14:30:14 +0800

    FIx deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/trade-prep/'
+  base: '/trade-prep/',
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
