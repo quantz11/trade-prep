e669b50669a50cab5e1e39aca995224f944d0443
Quantz <gdbayucan@live.com.ph>
2026-08-19 18:11:29 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import {defineConfig} from 'vite';
 
 export default defineConfig({
-  base: '/trade-prep/',
+  base: './',
   plugins: [
     react(),
     tailwindcss(),
   ],
+  resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
+    }
 })
